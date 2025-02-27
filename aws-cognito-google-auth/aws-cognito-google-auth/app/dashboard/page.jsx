1bbc2eb4fde523ae11e63e5a94e1a76b48645acb
Renzo Flestado <131667929+itsrenzosamaaa@users.noreply.github.com>
2025-02-27 21:20:15 +0800

    Delete aws-cognito-google-auth/aws-cognito-google-auth directory

--- aws-cognito-google-auth/aws-cognito-google-auth/app/dashboard/page.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { withAuth } from "../../hoc/withAuth";
-import EnhancedDashboard from "../../components/EnhancedDashboard";
-
-const ProtectedEnhancedDashboard = withAuth(EnhancedDashboard);
-
-export default function Dashboard() {
-    return <main><ProtectedEnhancedDashboard /></main>;
-}
--- aws-cognito-google-auth/aws-cognito-google-auth/src/utils/oidcConfigSignInConfig.js
+++
@@ -1,9 +0,0 @@
-const uri = process.env.ENV === "Dev" ? process.env.COGNITO_LOCAL_URL : process.env.COGNITO_PROD_URL;
-
-export const oidcConfigSignInConfig = {
-  authority: process.env.COGNITO_AUTHORITY, // MUST match Cognito Hosted UI Domain
-  client_id: process.env.COGNITO_CLIENT_ID,
-  redirect_uri: `${uri}/callback`, // MUST be allowed in Cognito
-  response_type: "code",
-  scope: "openid email profile aws.cognito.signin.user.admin",
-};
